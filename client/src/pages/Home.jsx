9e8671ca26179d6a239b00cfc9e425180bafe1a4
Your Name <sachinbhavar@gmail.com>
2025-02-18 21:36:12 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,20 +2,31 @@ import React, { useContext, useEffect } from "react";
 // import axiosInstance from "../api/axiosinstance";
 import { PollContext } from "../context/PollContext";
 import axios from "axios";
-import { configDotenv } from "dotenv";
-configDotenv();
+// import { configDotenv } from "dotenv";
+// configDotenv();
 
-const url = process.env.REACT_APP_API_URL;
+// const url = process.env.REACT_APP_API_URL;
 const Home = () => {
     const { currentPoll, setCurrentPoll } = useContext(PollContext);
 
     useEffect(() => {
-        const fetchPoll = async () => {
-            const responce = await axios.get(url);
-            setCurrentPoll(responce.data);
-            console.log(responce.data);
-        };
-        fetchPoll();
+        fetch("https://poll-app-7tos.onrender.com/", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((data) => data.json())
+            .then((res) => {
+                console.log(res.data);
+                setCurrentPoll(res.data);
+            });
+        // const fetchPoll = async () => {
+
+        //     const responce = await axios.get(url);
+        //
+        //     console.log(responce.data);
+        // };
+        // fetchPoll();
     }, []);
 
     return <div className="flex flex-col items-center">Homepage</div>;
