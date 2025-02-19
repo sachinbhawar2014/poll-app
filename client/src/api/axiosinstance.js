8323e6fb46559e950a38b1f4e0611b07834db7f5
Your Name <sachinbhavar@gmail.com>
2025-02-19 11:22:15 +0530

    update

--- client/src/api/axiosinstance.js
+++ client/src/api/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://poll-app-7tos.onrender.com",
+    baseURL: "https://poll-app-nies.onrender.com/",
     withCredentials: true, // Ensures cookies/session data is sent
     headers: {
         "Content-Type": "application/json",
